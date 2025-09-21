1a3d189a2bdc1edb53c08f13c4ff857487c9af13
Luthfi Ramdhani <blackidluthfi9@gmail.com>
2025-09-21 10:15:44 +0700

    Fix Netlify build: Move vite-plugin-static-copy to dependencies and simplify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/admin/**/*',
-          dest: 'admin'
-        },
-        {
-          src: 'content/**/*',
-          dest: 'content'
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
